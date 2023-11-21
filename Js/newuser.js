63dc6372de0444ca386fd818e02527bdb1e79d26
YmerA38 <stin166c@stud.kea.dk>
2023-11-21 12:55:04 +0100

    in progress

---
+++ Js/newuser.js
@@ -0,0 +1,25 @@
+import {
+    postData
+} from "./fetch";
+
+const userName = document.getElementById("username");
+const pwd = document.getElementById("password");
+const pwdRe = document.getElementById("re-password");
+const registerBtn = document.getElementById("register");
+const responseMess = document.getElementById("response");
+
+userName.innerText = "wright a username"
+pwd.innerText = "wright a password"
+pwdRe.innerText = "reenter password"
+
+registerBtn.addEventListener('click',()=>{
+    const user = {
+        userName : userName,
+        pwd : pwd,
+        role: "ROLE_ADMIN"
+    }
+    postData(user).then((resp)=> {
+        responseMess.innerText = resp
+    })
+})
+
