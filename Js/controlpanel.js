3473c04c324dfc8329efc3d85aa72d795a65ba4b
AmingYee <wenm0007@stud.kea.dk>
2023-12-11 03:12:09 +0100

    refactor

--- Js/controlpanel.js
+++ Js/controlpanel.js
@@ -5,14 +5,10 @@ import {
 document.addEventListener('DOMContentLoaded', () => {
     checkLogin();
 });
-const frame1 = document.getElementById("frame1")
+/*
 const frame2 = document.getElementById("frame2")
 const frame3 = document.getElementById("frame3")
 
-frame1.contentDocument.addEventListener("dblclick",()=>{
-    console.log("clicked")
-    window.location.href = frame1.getAttribute("src")
-})
 frame2.contentDocument.addEventListener("dblclick",()=>{
     console.log("clicked")
     window.location.href = frame2.getAttribute("src")
@@ -21,10 +17,7 @@ frame3.contentDocument.addEventListener("dblclick",()=>{
     console.log("clicked")
     window.location.href = frame3.getAttribute("src")
 })
-
-
-
-
+ */
 
 //------background
 function getRandomElement(array) {
@@ -33,11 +26,6 @@ function getRandomElement(array) {
 
 
 const backgroundImages = [
-    'https://live.staticflickr.com/65535/53360059100_4d2c9d2252_b.jpg',
-    'https://live.staticflickr.com/65535/53360526035_0e239fa940_b.jpg',
-    'https://live.staticflickr.com/65535/53359612511_caccdb7de9_b.jpg',
-    'https://live.staticflickr.com/65535/53358722222_5dbf280c44_b.jpg',
-    'https://live.staticflickr.com/65535/53360059095_c52d3b5787_b.jpg',
     'https://live.staticflickr.com/65535/53361891119_617f0fbf86_b.jpg'
 
 ];
@@ -45,5 +33,6 @@ const backgroundImages = [
 
 document.addEventListener('DOMContentLoaded', function () {
     const randomBackground = getRandomElement(backgroundImages);
+    console.log('Setting background image..')
     document.body.style.backgroundImage = `url('${randomBackground}')`
 })
--- Js/email.js
+++ Js/email.js
@@ -3,6 +3,18 @@ import {
     getEmailContent
 } from './fetch.js';
 
+const emailContentHeading = document.getElementById("emailContent");
+
+emailContentHeading.style.display = "none";
+
+function showEmailContent() {
+    emailContentHeading.style.display = "block";
+}
+
+function hideEmailContent() {
+    emailContentHeading.style.display = "none";
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     const toggleThemeButton = document.getElementById("toggleTheme");
     const body = document.body;
@@ -66,8 +78,6 @@ async function fetchEmailContent(emailId) {
             const contentDiv = document.getElementById("emailContentText");
 
             contentDiv.innerHTML = `
-                <p><strong>ID:</strong> ${emailContent.id}</p>
-
                             <p><i <i class="fa-solid fa-highlighter"></i> ${emailContent.subject}</p>
              <p><i class="fa-solid fa-user"></i> ${emailContent.fromAddress}</p>
                 <p><i class="fa-regular fa-calendar"></i> ${new Date(emailContent.sentDate).toLocaleString()}</p>
@@ -79,12 +89,14 @@ async function fetchEmailContent(emailId) {
             const listItem = emailList.querySelector(`[data-email-id="${emailId}"]`);
             if (listItem) {
                 listItem.innerHTML = `<i class="fa-regular fa-envelope-open icon-browner"></i> ${emailContent.subject}`;
+                showEmailContent();
             }
         } else {
             throw new Error('Invalid or empty response');
         }
     } catch (error) {
         console.error("Error fetching email content:", error);
+        hideEmailContent();
         throw error;
     }
 }
@@ -107,26 +119,60 @@ async function sortEmails(sortByNewest) {
             }
         });
 
+        //arrays with emails
+        const thisWeekEmails = [];
+        const olderEmails = [];
+
         // Updater listen med den opdaterede liste af emails
         const emailList = document.getElementById("emailList");
         emailList.innerHTML = "";
 
         sortedEmails.forEach(email => {
-            const listItem = document.createElement("li");
+            const sentDate = new Date(email.sentDate);
 
-            const iconClass = email.seen ? 'fa-envelope-open' : 'fa-envelope';
-            listItem.innerHTML = `<i class="fa-regular ${iconClass} icon-browner"></i> ${email.subject}`;
-
-            listItem.dataset.emailId = email.id;
-            listItem.addEventListener("click", () => fetchEmailContent(email.id));
-            emailList.appendChild(listItem);
+            if (isSameWeek(sentDate, new Date())) {
+                thisWeekEmails.push(email);
+            } else {
+                olderEmails.push(email);
+            }
         });
+
+        if (thisWeekEmails.length > 0) {
+            const weekHeader = document.createElement("li");
+            weekHeader.innerHTML = `<strong>This Week</strong>`;
+            emailList.appendChild(weekHeader);
+
+            thisWeekEmails.forEach(email => {
+                const listItem = document.createElement("li");
+                const iconClass = email.seen ? 'fa-envelope-open' : 'fa-envelope';
+                listItem.innerHTML = `<i class="fa-regular ${iconClass} icon-browner"></i> ${email.subject}`;
+                listItem.dataset.emailId = email.id;
+                listItem.addEventListener("click", () => fetchEmailContent(email.id));
+                emailList.appendChild(listItem);
+            });
+        }
+
+        if (olderEmails.length > 0) {
+            const oldHeader = document.createElement("li");
+            oldHeader.innerHTML = `<strong>Older Emails</strong>`;
+            emailList.appendChild(oldHeader);
+
+            olderEmails.forEach(email => {
+                const listItem = document.createElement("li");
+                const iconClass = email.seen ? 'fa-envelope-open' : 'fa-envelope';
+                listItem.innerHTML = `<i class="fa-regular ${iconClass} icon-browner"></i> ${email.subject}`;
+                listItem.dataset.emailId = email.id;
+                listItem.addEventListener("click", () => fetchEmailContent(email.id));
+                emailList.appendChild(listItem);
+            });
+        }
     } catch (error) {
         console.error("Error fetching and sorting emails:", error);
     }
 }
 
-document.addEventListener("dblclick", () => {
-    console.log("clicked");
-    window.location.href = "controlpanel.html";
-});
+function isSameWeek(date1, date2) {
+    const oneWeek = 7 * 24 * 60 * 60 * 1000; // 1 uge
+    const diff = Math.abs(date1 - date2);
+    return diff < oneWeek && date1.getDay() <= date2.getDay();
+}
